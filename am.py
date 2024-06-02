219376a819cbb2b20203905792be4b3c4343c988
dghfhdgbxgbg <134328176+dghfhdgbxgbg@users.noreply.github.com>
2024-06-02 17:28:28 +0530

    Update am.py

--- am.py
+++ am.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("rm -rf MusicBoT && git clone https://github.com/dghfhdgbxgbg/MusicBoT && cd MusicBoT && python3 -m Yone")
+system ("rm -rf MusicBoT && git clone https://github.com/dghfhdgbxgbg/MusicBoT && cd MusicBoT && python3 -m AMBOTMusicBoT")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
