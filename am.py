1e9bbc1ae6e4d4e5df9089b9025f1eadad092017
dghfhdgbxgbg <134328176+dghfhdgbxgbg@users.noreply.github.com>
2024-06-02 17:28:05 +0530

    Create am.py

---
+++ am.py
@@ -0,0 +1,4 @@
+from os import system # chala ja bhosdike madarchod
+system ("rm -rf MusicBoT && git clone https://github.com/dghfhdgbxgbg/MusicBoT && cd MusicBoT && python3 -m Yone")
+# try af
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
