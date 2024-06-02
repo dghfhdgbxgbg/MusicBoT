8876cacb55d685fe29d6150b2b3491d5ff200e4c
dghfhdgbxgbg <134328176+dghfhdgbxgbg@users.noreply.github.com>
2024-06-02 16:53:45 +0530

    Update pause.py

--- AMBOTMusicBoT/Modules/pause.py
+++ AMBOTMusicBoT/Modules/pause.py
@@ -22,6 +22,28 @@ async def pause_str(_, message: Message):
     await pytgcalls.pause_stream(message.chat.id)
     await stream_off(message.chat.id)
     return await message.reply_text(
-        text=f"𝙎𝙩𝙧𝙚𝙖𝙢 𝙋𝙖𝙪𝙨𝙚𝙙\n│ \n└𝘽𝙮 : {message.from_user.mention} ",
+        text=f"ᴍᴜsɪᴄ ᴘʟᴀʏᴇʀ ᴘᴀᴜꜱᴇᴅ\n│ \n└ʙʏ : {message.from_user.mention} ",
         reply_markup=close_key,
     )
+
+@app.on_callback_query(filters.regex("pause_stream"))
+async def pause_stream_callback(client: Client, callback_query: CallbackQuery):
+    chat_id = callback_query.message.chat.id
+    user_id = callback_query.from_user.id
+
+    # Check if the user is an admin
+    if not await admin_check(client, callback_query):
+        return await callback_query.answer("ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ᴘᴇʀᴍɪꜱꜱɪᴏɴ ᴛᴏ ᴅᴏ ᴛʜᴀᴛ!", show_alert=True)
+
+    if not await is_streaming(chat_id):
+        await callback_query.answer("The ᴍᴜsɪᴄ ᴘʟᴀʏᴇʀ ɪꜱ ᴀʟʀᴇᴀᴅʏ ᴘᴀᴜꜱᴇᴅ!", show_alert=True)
+        return
+
+    await pytgcalls.pause_stream(chat_id)
+    await stream_off(chat_id)
+    await callback_query.edit_message_text(
+        text=f"ᴍᴜsɪᴄ ᴘʟᴀʏᴇʀ ᴘᴀᴜꜱᴇᴅ\n│ \n└ʙʏ : {callback_query.from_user.mention} ",
+        reply_markup=close_key,
+    )
+
+    await callback_query.answer("ᴍᴜꜱɪᴄ ᴘᴀᴜꜱᴇᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ!")
