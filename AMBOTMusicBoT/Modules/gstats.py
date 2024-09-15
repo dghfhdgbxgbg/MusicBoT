dda8c5c57235028a5ce8d1c23b953c60670afea6
dghfhdgbxgbg <134328176+dghfhdgbxgbg@users.noreply.github.com>
2024-09-15 13:17:30 +0530

    Create gstats.py

---
+++ AMBOTMusicBoT/Modules/gstats.py
@@ -0,0 +1,24 @@
+import platform
+from sys import version as pyver
+
+import psutil
+from pyrogram import __version__ as pyrover
+from pyrogram import filters
+from pyrogram.errors import MessageIdInvalid
+from pyrogram.types import InputMediaPhoto, Message
+from pytgcalls.__version__ import __version__ as pytgver
+
+import config
+from AMBOTMusicBoT import app
+from AMBOTMusicBoT.database.db import get_served_chats, get_served_users
+from config import BANNED_USERS, PING_IMG
+
+
+@app.on_message(filters.command(["gstats"]) & filters.group)
+async def stats_global(client, message: Message, _):
+    served_chats = len(await get_served_chats())
+    served_users = len(await get_served_users())
+    await message.reply_photo(
+        photo=PING_IMG,
+        caption= f"served chats = {served_chats}\nserved users = {served_users}"
+    )
