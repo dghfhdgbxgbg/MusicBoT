0163d6e1348439cc2f0a5f25eab7824540093656
dghfhdgbxgbg <134328176+dghfhdgbxgbg@users.noreply.github.com>
2024-09-15 13:11:06 +0530

    Update mongo.py

--- AMBOTMusicBoT/Helpers/mongo.py
+++ AMBOTMusicBoT/Helpers/mongo.py
@@ -2,7 +2,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 from config import MONGO_DB_URI
 
-from AMBOTMusicBoT import LOGGER
+from AMBOTMusicBoT.logging.logging import LOGGER
 
 LOGGER(__name__).info("Connecting to your Mongo Database...")
 try:
